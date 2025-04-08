407a2c0a15315da68252528f43b0c4b17cdd7f9c
zadumka <90329284+zadumka@users.noreply.github.com>
2025-04-08 13:09:23 +0300

    Update task-2.ts

--- src/task-2.ts
+++ src/task-2.ts
@@ -4,10 +4,12 @@ const product: Product = {
   description: "Compact and fast",
 };
 
-interface Product {
-  id: number;
+type Product = {
+  readonly id: number;
   title: string;
   description?: string;
-}
+};
 
 console.log(`Product: ${JSON.stringify(product)}`);
+
+
